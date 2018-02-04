03bbea45448ba4f46e9114561e77080f495050db
fiatjaf <fiatjaf@gmail.com>
2018-02-04 10:24:25 -0200

    the widget on the site done well.

--- site/app.js
+++ site/app.js
@@ -1,8 +1,4 @@
 const component = require('../component')
 
-let lessPass = document.createElement('div')
-let shadow = lessPass.attachShadow({mode: 'closed'})
-shadow.appendChild(component.style)
-shadow.appendChild(component.start())
-
-document.body.appendChild(lessPass)
+document.head.appendChild(component.style)
+document.getElementById('widget-here').appendChild(component.start())
